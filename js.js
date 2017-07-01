903d9525c7376990b3dbe460e9d35162bd5e3b58
ilyaqwerty <topmastertop97@gmail.com>
2017-07-01 16:21:45 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,9 +2,11 @@ var l = document.getElementById("languages");
 var s = document.getElementById("social");
 var soc = document.getElementById("socwrap");
 
+
+
 l.onmouseover = function(){
 	this.style.transition = "width  0.7s  ease-out";
-	this.style.background = "#999";
+	this.style.background = "#666";
 	this.style.width = "240px";
 }
 
@@ -15,16 +17,18 @@ l.onmouseout = function(){
 }
 
 s.onmouseover = function(){
-	soc.style.transition = "width  0.7s  ease-out";
-	soc.style.background = "#999";
+	soc.style.transition = "width  0.7s  ease-out, opacity 0.3s ease-out, background 0.5s ease, border 0.7s ease";
+	soc.style.background = "#666";
 	soc.style.border = "3px #000 solid";
 	soc.style.width = "140px";
+	soc.style.opacity = "1"
 }
 
 s.onmouseout = function(){
-	soc.style.transition = "width  0.7s  ease-out";
+	soc.style.transition = "width  0.7s  ease-out, opacity 0.5s ease-out, background 0.5s ease, border 0.7s ease";
 	soc.style.background = "transparent";
 	soc.style.border = "3px transparent solid";
 	soc.style.width = "0px";
+	soc.style.opacity = "0"
 }
 
