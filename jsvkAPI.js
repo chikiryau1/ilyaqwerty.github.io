c3e91e65ec75f6b81ef2619a5349fa722f3a3c98
naruto <topmastertop97@gmail.com>
2017-07-06 16:52:15 +0300

    w

--- jsvkAPI.js
+++ jsvkAPI.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
   	url: "vkAPIbd.json",
   	success: function(data) {
 	console.log(data);
-	for(var key in data.response){
-		for(var key1 in data.response[key]){
+		for(var key1 in data.response){
 			for(var key2 in data.response[key][key1]){
-			e += "<a href = "+data.response[key][key1][key2]["src"]+">"+data.response[key][key1][key2]["src"]+"</a><br>";
+			e += "<a href = "+data.response[key1][key2]["src"]+">"+data.response[key1][key2]["src"]+"</a><br>";
 		}
-	}
+
 	}
 	$( ".link" ).html(e);
   	}
