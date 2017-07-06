13365f694ebeca033811cac877f7ed96138b27f0
ilyaqwerty <topmastertop97@gmail.com>
2017-07-06 17:38:55 +0300

    Update jsvkAPI.js

--- jsvkAPI.js
+++ jsvkAPI.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 	var e = "";
 	$.ajax({
 	type: "GET",
-	
+	data: {
+		owner_id: "-55438333",
+		album_id: "wall",
+		count:"1000"
+	},
   	dataType: "json",
-  	url: "https://api.vk.com/method/photos.get?owner_id=-55438333&album_id=wall&count=1000",
+  	url: "https://api.vk.com/method/photos.get",
   	success: function(data) {
 	console.log(data);
 		for(var key in data.response){
