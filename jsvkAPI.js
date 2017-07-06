bd1ac43c16228af92ea614e5aa4892a8dfd8a3b2
ilyaqwerty <topmastertop97@gmail.com>
2017-07-06 17:16:57 +0300

    Update jsvkAPI.js

--- jsvkAPI.js
+++ jsvkAPI.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   	success: function(data) {
 	console.log(data);
 		for(var key in data.response){
-			e += "<img src = "+data.response[key]["src"]+">"+data.response[key]["src"] + ">";
+			e += "<img src = "+data.response[key]["src"]+">";
 			}
 	$( ".link" ).html(e);
   	}
