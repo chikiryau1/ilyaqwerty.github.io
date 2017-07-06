e9f969d30338df66931d014350ed04f1bd4ec858
ilyaqwerty <topmastertop97@gmail.com>
2017-07-06 17:19:40 +0300

    Update jsvkAPI.js

--- jsvkAPI.js
+++ jsvkAPI.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	var e = "";
 	$.ajax({
+	type: GET,
+	
   	dataType: "json",
-  	url: "vkAPIbd.json",
+  	url: "https://api.vk.com/method/photos.get?owner_id=-55438333&album_id=wall&count=1000",
   	success: function(data) {
 	console.log(data);
 		for(var key in data.response){
